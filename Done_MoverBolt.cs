76a07ef71fed6a6c47fcb710c40f0113fdceaca4
Digvijay Anand <36638107+digvijay027@users.noreply.github.com>
2018-06-25 01:11:50 +0530

    Add files via upload

---
+++ Done_MoverBolt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Done_MoverBolt : MonoBehaviour {
+
+	public float speed;
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody> ().velocity = transform.up * speed;
+	}
+}
